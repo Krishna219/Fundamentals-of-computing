682bf3e2bdb9110d7a60eb919b52db317bec2605
Krishna Srinivasan <satheshkishore@gmail.com>
2020-07-25 20:51:36 +0530

    IIPP WK 0a Expressions

---
+++ An Introduction to Interactive Programming in Python/Week 0a - Expressions/Expressions (Week 0a).py
@@ -0,0 +1,29 @@
+#1 Mile to feet Convertion of 13 Miles
+print 13*5280 
+
+#2 Display equivalent seconds of 07:21:37
+print '\n',7*60*60+21*60+37 
+
+#3 Perimeter of the rectangle with sides 7 inches and 4 inches
+print '\n',2*(7+4) 
+
+#4  area of the same rectangle
+print '\n',7*4  
+
+#5 perimeter of a circle with radius 8 inches
+print '\n',2*3.14*8 
+
+#6 area of the same circle
+print '\n',3.14*8**2  
+
+#7 compound interest for $1000 at the rate of 7% after 10 years
+print '\n',1000*(1+0.01*7)**10 
+
+#8 String concatenation
+print '\n','My name is'+' joe'+' warren' 
+
+#9 int to str convertion
+print '\n','Joe Warren is '+str(52)+' Years old' 
+
+#10 Distance between points (2,2) & (5,6)
+print '\n',((2-5)**2+(2-6)**2)**(1.0/2.0) 
---
+++ An Introduction to Interactive Programming in Python/Week 0a - Expressions/Practice Programs Week 0a.py
@@ -0,0 +1,10 @@
+print 13*5280 # Mile to feet Convertion of 13 Miles
+print 7*60*60+21*60+37 # Display equivalent seconds of 07:21:37
+print 2*(7+4) #Perimeter of the rectangle with sides 7 inches and 4 inches
+print 7*4 # area of the same rectangle 
+print 2*3.14*8 # perimeter of a circle with radius 8 inches
+print 3.14*8**2 # area of the same circle 
+print 1000*(1+0.01*7)**10 #compound interest for $1000 at the rate of 7% after 10 years
+print 'My name is'+' joe'+' warren' # String concatenation
+print 'Joe Warren is '+str(52)+' Years old' # int to str convertion
+print ((2-5)**2+(2-6)**2)**(1.0/2.0) # Distance between points (2,2) & (5,6)
